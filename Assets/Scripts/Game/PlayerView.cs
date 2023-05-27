5242732a29f0d8d897ef78286226bff6cc1a19fb
Vito Scaletta <lyaguxmaslo@gmail.com>
2023-05-27 14:39:21 +0300

    Updated Game scene * Also added Relay

--- Assets/Scripts/Game/PlayerView.cs
+++ Assets/Scripts/Game/PlayerView.cs
@@ -1,9 +1,18 @@
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
 namespace Runtime
 {
     public class PlayerView : MonoBehaviour
     {
+        private Vector3 _moveDir = Vector3.zero;
 
+        public void Move(Vector3 dir) => _moveDir = dir;
+
+        private void Start()
+        {
+
+        }
     }
 }
---
+++ Assets/Scripts/Game/SceneView.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using TMPro;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay.Models;
+using UnityEngine;
+
+namespace Runtime
+{
+    public class SceneView : MonoBehaviour
+    {
+        [field: SerializeField] private TextMeshProUGUI codeTmp;
+        [field: SerializeField] private int MaxConnections { get; set; }
+
+        private void Start()
+        {
+            SetJoinCode();
+        }
+
+        private async void SetJoinCode()
+        {
+            codeTmp.text = "Hello world";
+
+            string joinCode = await GetJoinCode(MaxConnections);
+
+            codeTmp.text = joinCode;
+        }
+
+        public static async Task<string> GetJoinCode(int maxConn)
+        {
+            await UnityServices.InitializeAsync();
+
+            if (AuthenticationService.Instance.IsSignedIn == false)
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+            Allocation allocation = await Unity.Services.Relay.RelayService.Instance.CreateAllocationAsync(maxConn);
+
+            var joinCode = await Unity.Services.Relay.RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            return joinCode;
+        }
+    }
+}
