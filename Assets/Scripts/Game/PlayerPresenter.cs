aed5c495a7d686113e160f5969a4de247bdb2f5c
Vito Scaletta <lyaguxmaslo@gmail.com>
2023-05-27 17:14:53 +0300

    Fixed a bug with jumping

--- Assets/Scripts/Game/PlayerPresenter.cs
+++ Assets/Scripts/Game/PlayerPresenter.cs
@@ -14,16 +14,42 @@ namespace Runtime
             ConfigureInput(inputWrapper);
         }
 
-        public void Update()
-        {
-
-        }
-
         public void MoveLeft() => Move(Vector3.left * _view.Configuration.MoveSpeed);
         public void MoveRight() => Move(Vector3.right * _view.Configuration.MoveSpeed);
-        public void Jump() => _view.Rigidbody.velocity = Vector2.up * _view.Configuration.JumpForce;
+        public void Jump()
+        {
+            if (IsOnGround() == false)
+                return;
 
-        private void Move(Vector3 dir) => _view.MoveDir += dir * Time.deltaTime;
+            _view.Rigidbody.velocity = Vector2.up * _view.Configuration.JumpForce;
+        }
+
+        public bool IsOnGround()
+        {
+            // Collider size / 1.99 - Collider radius with a little indent
+            var from = _view.transform.position + Vector3.down * (_view.Collider.bounds.size.y / 1.99f);
+
+            Debug.DrawLine(from, from + (Vector3.down * 0.1f));
+
+            return Physics2D.Raycast(from, Vector2.down, 0.1f);
+        }
+
+        private void Move(Vector3 dir)
+        {
+            _view.MoveDir += dir * Time.deltaTime;
+
+            if (dir.x > 0)
+                _view.TargetScale = _view.InitialScale;
+
+            else
+            {
+                var newScale = _view.InitialScale;
+
+                newScale.x *= -1;
+
+                _view.TargetScale = newScale;
+            }
+        }
 
         private void ConfigureInput(InputWrapper inputWrapper)
         {
--- Assets/Scripts/Game/PlayerView.cs
+++ Assets/Scripts/Game/PlayerView.cs
@@ -5,21 +5,28 @@ namespace Runtime
     public sealed class PlayerView : MonoBehaviour
     {
         [field: SerializeField] internal PlayerConfiguration Configuration { get; set; } = new();
+        [field: SerializeField] internal Transform ModelTransform { get; set; }
         [field: SerializeField] internal Rigidbody2D Rigidbody { get; set; }
+        [field: SerializeField] internal Collider2D Collider { get; set; }
         [field: SerializeField] internal Vector3 MoveDir { get; set; }
+        internal Vector3 InitialScale { get; private set; }
+        internal Vector3 TargetScale { get; set; }
         [field: SerializeField] private InputWrapper InputWrapper { get; set; }
         private PlayerPresenter _presenter;
 
         private void Start()
         {
+            InitialScale = TargetScale = ModelTransform.localScale;
             _presenter = new(this, InputWrapper);
         }
 
         private void Update()
         {
-            _presenter.Update();
-
             transform.position += MoveDir;
+
+            ModelTransform.localScale = Vector3.LerpUnclamped(ModelTransform.localScale,
+                TargetScale,
+                Configuration.RotationSpeed);
         }
 
         private void LateUpdate() => ResetValues();
